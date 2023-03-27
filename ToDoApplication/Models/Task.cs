9fb862f699bab4c4366d7f6d6afac4277d20ff39
Wojciech Pakoca <wojtekpako2@gmail.com>
2023-03-27 21:16:39 +0200

    Task Model

---
+++ ToDoApplication/Models/Task.cs
@@ -0,0 +1,23 @@
+﻿namespace ToDoApplication.Models
+{
+    public class Task
+    {
+        public int TaskId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public Category Category { get; set; }
+        public Status Status { get; set; }
+
+
+    }
+
+    public enum Category
+    {
+        DeepWork, ShallowWork, Chores, Learning, MindCare, BodyCare, People
+    }
+    public enum Status
+    {
+        ToDo, InProgress, Done
+    }
+}
