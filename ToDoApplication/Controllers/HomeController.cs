8536d3035282897e91584083c8215cb78b347682
Wojciech Pakoca <wojtekpako2@gmail.com>
2023-03-27 19:40:03 +0200

    Initial Commit

---
+++ ToDoApplication/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
---
+++ ToDoApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ToDoApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
