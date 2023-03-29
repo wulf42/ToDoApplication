430d60aa5afec22646aa5724bdb5bab46e43d877
Wojciech Pakoca <wojtekpako2@gmail.com>
2023-03-29 13:54:14 +0200

    DbContext

---
+++ ToDoApplication/Context/ToDoApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ToDoApplication.Context
+{
+    public class ToDoApplicationDbContext : DbContext
+    {
+        public ToDoApplicationDbContext(DbContextOptions<ToDoApplicationDbContext> options) : base(options) { }
+
+        //DbSets
+        public DbSet<Models.TaskToDo> TasksToDo { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
