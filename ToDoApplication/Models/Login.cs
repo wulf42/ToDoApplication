bda42b373ec215507f3ada444eb124c9fdd8deb2
Wojciech Pakoca <wojtekpako2@gmail.com>
2023-04-04 15:52:28 +0200

    Register and Login Models

---
+++ ToDoApplication/Models/Login.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoApplication.Models
+{
+    public class Login
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
