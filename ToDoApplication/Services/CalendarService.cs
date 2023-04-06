93723f113a6f29994776051aa0c51981a8a4ec7e
Wojciech Pakoca <wojtekpako2@gmail.com>
2023-04-06 18:33:27 +0200

    Fix custom event class + filter by user

--- ToDoApplication/Services/CalendarService.cs
+++ ToDoApplication/Services/CalendarService.cs
@@ -6,26 +6,29 @@ namespace ToDoApplication.Services
     public class CalendarService : ICalendarService
     {
         private readonly ToDoApplicationDbContext _context;
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
-
-        public CalendarService(ToDoApplicationDbContext context)
+        public CalendarService(ToDoApplicationDbContext context, IHttpContextAccessor httpContextAccessor)
         {
             _context = context;
+            _httpContextAccessor = httpContextAccessor;
         }
 
         public List<CustomCalendarEvent> GetCalendarEvents()
         {
-            var tasks = _context.TasksToDo.ToList();
+            var userId = _httpContextAccessor.HttpContext.Session.GetString("UserId");
+            var tasks = _context.TasksToDo.Where(t => t.addedBy == userId).ToList();
+
             var events = new List<CustomCalendarEvent>();
             foreach (var task in tasks)
             {
                 var calendarEvent = new CustomCalendarEvent
                 {
-                    Id = task.TaskId,
-                    Title = task.Name,
-                    Start = new DateTime(task.Date.Year, task.Date.Month, task.Date.Day, task.Time.Hour, task.Time.Minute, task.Time.Second),
-                    End = new DateTime(task.Date.Year, task.Date.Month, task.Date.Day, task.Time.Hour, task.Time.Minute, task.Time.Second).AddHours(1),
-                    AllDay = false,
+                    id = task.TaskId,
+                    title = task.Name,
+                    start = new DateTime(task.Date.Year, task.Date.Month, task.Date.Day, task.Time.Hour, task.Time.Minute, task.Time.Second),
+                    end = new DateTime(task.Date.Year, task.Date.Month, task.Date.Day, task.Time.Hour, task.Time.Minute, task.Time.Second).AddHours(1),
+                    allDay = false,
 
                 };
                 events.Add(calendarEvent);
@@ -34,12 +37,12 @@ namespace ToDoApplication.Services
         }
         public class CustomCalendarEvent
         {
-            public int Id { get; set; }
-            public string Title { get; set; }
-            public DateTime Start { get; set; }
-            public DateTime End { get; set; }
-            public bool AllDay { get; set; }
-            public string Color { get; set; }
+            public int id { get; set; }
+            public string title { get; set; }
+            public DateTime start { get; set; }
+            public DateTime end { get; set; }
+            public bool allDay { get; set; }
+            public string color { get; set; }
         }
     }
 
