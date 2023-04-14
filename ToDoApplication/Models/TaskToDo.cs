85c8529996da364d736ffa87071f6f3e90e0db30
Wojciech Pakoca <wojtekpako2@gmail.com>
2023-04-14 12:28:55 +0200

    Model for shopping list

--- ToDoApplication/Models/TaskToDo.cs
+++ ToDoApplication/Models/TaskToDo.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ToDoApplication.Models
 {
@@ -18,16 +19,28 @@ namespace ToDoApplication.Models
         public Category Category { get; set; }
 
         public Status Status { get; set; }
+        public List<ShoppingList>? shoppingLists { get; set; }
         public string? addedBy { get; set; }
     }
 
     public enum Category
     {
-        DeepWork, ShallowWork, Chores, Learning, MindCare, BodyCare, People, Other
+        DeepWork, ShallowWork, Chores, Learning, MindCare, BodyCare, People, ShoppingList, Other
     }
 
     public enum Status
     {
         ToDo, InProgress, Done
+    }
+
+    public class ShoppingList
+    {
+        public int productId { get; set; }        
+        public string name { get; set; }
+        public int quantity { get; set; }
+
+        [ForeignKey("TaskToDo")]
+        public int TaskToDoId { get; set; }
+        public TaskToDo TaskToDo { get; set; }
     }
 }
