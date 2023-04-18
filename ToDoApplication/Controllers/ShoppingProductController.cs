b4aaa5197f384614e3830fe6d01c89990f9b5442
Wojciech Pakoca <wojtekpako2@gmail.com>
2023-04-18 12:30:28 +0200

    ShoppingProductController

---
+++ ToDoApplication/Controllers/ShoppingProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoApplication.Controllers
+{
+    public class ShoppingProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
