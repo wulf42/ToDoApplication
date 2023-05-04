637243caa46df0fa77e2a1a31eaacb0604ccc830
Wojciech Pakoca <wojtekpako2@gmail.com>
2023-05-04 14:56:38 +0200

    Account viewmodel for change/forgot password

---
+++ ToDoApplication/ViewModels/AccountViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoApplication.ViewModels
+{
+    public class ForgotPasswordViewModel
+    {
+        public string Email { get; set; }
+
+
+    }
+    public class ResetPasswordViewModel
+    {
+        public string Email { get; set; }
+        public string Token { get; set; }
+    }
+    public class ChangePasswordViewModel
+    {
+        public string UserId { get; set; }
+        public string Token { get; set; }
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+
+}
