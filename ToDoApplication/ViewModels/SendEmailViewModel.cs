1d5afb3cf72ca4b6fbd6c3e39baa05b383278a5e
Wojciech Pakoca <wojtekpako2@gmail.com>
2023-05-23 18:52:14 +0200

    SendEmailViewModel

---
+++ ToDoApplication/ViewModels/SendEmailViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDoApplication.ViewModels
+{
+    public class SendEmailViewModel
+    {
+        public string Email { get; set; }
+        public string CallbackUrl { get; set; }
+        public string Subject { get; set; }
+        public string Template { get; set; }
+    }
+}
