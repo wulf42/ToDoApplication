6ad88e641e3c63492e0671f1af22b5cb2e6e58b8
Wojciech Pakoca <wojtekpako2@gmail.com>
2023-06-05 15:01:17 +0200

    Admin Panel Controller- get users and their roles

--- ToDoApplication/Controllers/AdminPanelController.cs
+++ ToDoApplication/Controllers/AdminPanelController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using ToDoApplication.Models;
+using ToDoApplication.ViewModels;
 
 namespace ToDoApplication.Controllers
 {
@@ -9,6 +12,38 @@ namespace ToDoApplication.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+    }
+
+    public class ManageUsersController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+
+        public ManageUsersController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            var users = _userManager.Users.ToList();
+
+            var model = new List<UserRolesViewModel>();
+
+            foreach (var user in users)
+            {
+                var role = _userManager.GetRolesAsync(user).GetAwaiter().GetResult().FirstOrDefault();
+
+                var userRolesViewModel = new UserRolesViewModel
+                {
+                    User = user,
+                    Role = role
+                };
+
+                model.Add(userRolesViewModel);
+            }
+
+            return View(model);
         }
     }
 }
